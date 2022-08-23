daa89c3aaad872a79aaebfc4af730005e2735d65
Sk-sopon <sksoponsk0@gmail.com>
2022-08-23 11:28:56 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Home from './components/home/Home';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
